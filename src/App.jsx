5a48c734abc64c2428f1d4c55407a99cfb7fea4f
Erhan99 <146257442+Erhan99@users.noreply.github.com>
2025-07-31 23:47:13 +0200

    projects and smooth scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Home from "./Home"
 import Technologies from "./Technologies"
+import Projects from "./Projects"
+import {Scrollbar} from 'smooth-scrollbar-react';
 
 function App() {
   return (
-     <>
-        <Home />
-        <Technologies />
-    </>
+     <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+        <Scrollbar
+          damping={0.1}
+          thumbMinSize={30}
+          renderByPixels={true}
+          alwaysShowTracks={true}
+           continuousScrolling={true}
+           style={{ height: "100vh" }}
+        >
+          <Home />
+          <Technologies />
+          <Projects />
+        </Scrollbar>
+    </div>
   )
 }
 
