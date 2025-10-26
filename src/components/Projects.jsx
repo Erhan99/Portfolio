c6b65b064c6b9fcfbf6851b08c9c69673bf520a2
Erhan99 <146257442+Erhan99@users.noreply.github.com>
2025-10-26 13:10:14 +0100

    project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,39 +1,54 @@
-import MySocial from '../assets/MySocial_Mockup.png';
-import BudgetTracker from '../assets/BudgetTracker_Mockup.png';
-import AICode from '../assets/AICode_Mockup.png';
-import Project from './Project.jsx';
+import MySocial from "../assets/MySocial_Mockup.png";
+import BudgetTracker from "../assets/BudgetTracker_Mockup.png";
+import AICode from "../assets/AICode_Mockup.png";
+import Game from "../assets/game.png";
+import Project from "./Project.jsx";
 
-export default function Projects() { 
-   return (
-      <section className="container mt-5">
-         <h2 className="title fw-bold">Latest Projects</h2>
-         <p className='text-white'>Here are some of the projects I've worked on:</p>
-         <div className='d-flex flex-wrap justify-content-center mt-4 gap-4'>
+export default function Projects() {
+  return (
+    <section className="container mt-5">
+      <h2 className="title fw-bold">Latest Projects</h2>
+      <p className="text-white">
+        Here are some of the projects I've worked on:
+      </p>
+      <div className="d-flex flex-wrap justify-content-center mt-4 gap-4">
+        <Project
+          img={MySocial}
+          title="MySocial"
+          tags={[
+            "C#",
+            "ASP.NET",
+            "SQL Server",
+            "Entity Framework",
+            "Javascript",
+            "Bootstrap",
+          ]}
+          text="A social media platform built with C# and ASP.NET."
+          url="https://github.com/Erhan99/MySocial"
+        />
 
-            <Project
-               img={MySocial}
-               title="MySocial"
-               tags={["C#", "ASP.NET", "SQL Server", "Entity Framework", "Javascript", "Bootstrap"]}
-               text="A social media platform built with C# and ASP.NET."
-               url="https://github.com/Erhan99/MySocial"
-            />
+        <Project
+          img={BudgetTracker}
+          title="Budget Tracker"
+          tags={["Kotlin", "Java", "Jetpack Compose", "PHP", "MySQL"]}
+          text="A budgeting application built with Kotlin and Jetpack Compose."
+          url="https://github.com/Erhan99/AWM-BudgetTracker-v2"
+        />
 
-            <Project
-               img={BudgetTracker}
-               title="Budget Tracker"
-               tags={["Kotlin", "Java", "Jetpack Compose", "PHP", "MySQL"]}
-               text="A budgeting application built with Kotlin and Jetpack Compose."
-               url="https://github.com/Erhan99/AWM-BudgetTracker-v2"
-            />
-
-            <Project
-               img={AICode}
-               title="AI Code Explainer"
-               tags={["React", "Flask", "Langchain", "Bootstrap"]}
-               text="A local AI-powered code explanation tool built with React and Flask."
-            />
-         </div>
-      </section>
-   );
+        <Project
+          img={AICode}
+          title="AI Code Explainer"
+          tags={["React", "Flask", "Langchain", "Bootstrap"]}
+          text="A local AI-powered code explanation tool built with React and Flask."
+        />
+        <Project
+          img={Game}
+          title="Monster Blitz"
+          tags={["Unity", "C#"]}
+          text="A round based game where the player has to survive as many rounds as possible against monsters."
+          url="https://github.com/Erhan99/MonsterBlitzGame"
+        />
+      </div>
+    </section>
+  );
 }
-               
